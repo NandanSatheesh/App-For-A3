ff771f91371cc93ad27a324ae7898839845f5562
Nandan Satheesh <nandan.199776@gmail.com>
2019-04-10 00:35:48 +0530

    Minor Changes

--- app/src/main/java/com/project/application/FirstPage.java
+++ app/src/main/java/com/project/application/FirstPage.java
@@ -3,6 +3,7 @@ package com.project.application;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,8 @@ public class FirstPage extends Activity {
 	Button loginAction;
 	EditText userName, password;
 
-	protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 
